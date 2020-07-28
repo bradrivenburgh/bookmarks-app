d2663886486072e8b98316a582f2ce8a69af63c2
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-07-28 15:40:30 -0400

    added React Router and elminated conditional rendering

--- src/AddBookmark/AddBookmark.test.js
+++ src/AddBookmark/AddBookmark.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import AddBookmark from './AddBookmark';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<AddBookmark />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+      <AddBookmark />
+    </BrowserRouter>, 
+    div);
   ReactDOM.unmountComponentAtNode(div);
 });
