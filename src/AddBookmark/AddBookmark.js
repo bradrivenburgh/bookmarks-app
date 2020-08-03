3a7008ecd3e1307b8bf41a564b41293a92adf15d
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-08-03 14:21:22 -0400

    added context

--- src/AddBookmark/AddBookmark.js
+++ src/AddBookmark/AddBookmark.js
@@ -1,6 +1,6 @@
 import React, { Component } from  'react';
-import { withRouter } from 'react-router-dom';
-import config from '../config'
+import config from '../config';
+import BookmarksContext from '../BookmarksContext';
 import './AddBookmark.css';
 
 const Required = () => (
@@ -8,13 +8,10 @@ const Required = () => (
 )
 
 class AddBookmark extends Component {
-  static defaultProps = {
-    onAddBookmark: () => {}
-  };
-
   state = {
     error: null,
   };
+  static contextType = BookmarksContext;
 
   handleSubmit = e => {
     e.preventDefault()
@@ -51,16 +48,19 @@ class AddBookmark extends Component {
         description.value = ''
         rating.value = ''
         this.props.history.push('/')
-        this.props.onAddBookmark(data)
+        this.context.addBookmark(data)
       })
       .catch(error => {
         this.setState({ error })
       })
   }
 
+  handleClickCancel = () => {
+    this.props.history.push('/')
+  };
+
   render() {
     const { error } = this.state
-    const { onClickCancel } = this.props
     return (
       <section className='AddBookmark'>
         <h2>Create a bookmark</h2>
@@ -125,7 +125,7 @@ class AddBookmark extends Component {
             />
           </div>
           <div className='AddBookmark__buttons'>
-            <button type='button' onClick={onClickCancel}>
+            <button type='button' onClick={this.handleClickCancel}>
               Cancel
             </button>
             {' '}
@@ -139,4 +139,4 @@ class AddBookmark extends Component {
   }
 }
 
-export default withRouter(AddBookmark);
+export default AddBookmark;
--- src/App.js
+++ src/App.js
@@ -2,37 +2,14 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import AddBookmark from './AddBookmark/AddBookmark';
 import BookmarkList from './BookmarkList/BookmarkList';
+import BookmarksContext from './BookmarksContext'
 import Nav from './Nav/Nav';
 import config from './config';
 import './App.css';
 
-const bookmarks = [
-  // {
-  //   id: 0,
-  //   title: 'Google',
-  //   url: 'http://www.google.com',
-  //   rating: '3',
-  //   desc: 'Internet-related services and products.'
-  // },
-  // {
-  //   id: 1,
-  //   title: 'Thinkful',
-  //   url: 'http://www.thinkful.com',
-  //   rating: '5',
-  //   desc: '1-on-1 learning to accelerate your way to a new high-growth tech career!'
-  // },
-  // {
-  //   id: 2,
-  //   title: 'Github',
-  //   url: 'http://www.github.com',
-  //   rating: '4',
-  //   desc: 'brings together the world\'s largest community of developers.'
-  // }
-];
-
 class App extends Component {
   state = {
-    bookmarks,
+    bookmarks: [],
     error: null,
   };
 
@@ -49,6 +26,15 @@ class App extends Component {
     })
   }
 
+  deleteBookmark = bookmarkId => {
+      const newBookmarks = this.state.bookmarks.filter(bm =>
+        bm.id !== bookmarkId
+      )
+      this.setState({
+        bookmarks: newBookmarks
+      })
+    }
+    
   componentDidMount() {
     fetch(config.API_ENDPOINT, {
       method: 'GET',
@@ -68,30 +54,27 @@ class App extends Component {
   }
 
   render() {
-    const { bookmarks } = this.state
+    const contextValue = {
+      bookmarks: this.state.bookmarks,
+      addBookMark: this.addBookmark,
+      deleteBookmark: this.deleteBookmark
+    }
     return (
       <main className='App'>
         <h1>Bookmarks!</h1>
-        <Nav />
-        <div className='content' aria-live='polite'>
-          <Route
-            path='/add-bookmark'
-            render={({history}) => {
-              console.log(history);
-              return <AddBookmark
-                onAddBookmark={this.addBookmark}
-                onClickCancel={() => history.push('/')}
-              />
-            }}
-          />
-          <Route
-            exact path='/'
-            render={() =>
-              <BookmarkList 
-                bookmarks={bookmarks}
-              />}
-          /> 
-        </div>
+        <BookmarksContext.Provider value={contextValue}>
+          <Nav />
+          <div className='content' aria-live='polite'>
+            <Route
+              path='/add-bookmark'
+              component={AddBookmark}
+            />
+            <Route
+              exact path='/'
+              component={BookmarkList}
+            /> 
+          </div>
+        </BookmarksContext.Provider>
       </main>
     );
   }
