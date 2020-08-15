6a2a4afacfa6cf8918bd812490b4543c0d4b9165
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-08-15 13:48:38 -0400

    adding propTypes

--- src/BookmarkList/BookmarkList.js
+++ src/BookmarkList/BookmarkList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import BookmarksContext from '../BookmarksContext';
 import BookmarkItem from '../BookmarkItem/BookmarkItem';
 import './BookmarkList.css'
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class BookmarkList extends Component {
   static defaultProps = {
@@ -32,10 +32,29 @@ class BookmarkList extends Component {
 export default BookmarkList;
 
 BookmarkList.propTypes = {
-  bookmarks: propTypes.arrayOf(propTypes.shape({
-    title: propTypes.string.isRequired,
-    url: propTypes.string.isRequired,
-    rating: propTypes.number,
-    description: propTypes.string
+  bookmarks: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    url: (props, propName, componentName) => {
+      // get the value of the prop
+      const prop = props[propName];
+  
+      // do the isRequired check
+      if(!prop) {
+        return new Error(`${propName} is required in ${componentName}. Validation Failed`);
+      }
+  
+      // check the type
+      if (typeof prop != 'string') {
+        return new Error(`Invalid prop, ${propName} is expected to be a string in ${componentName}. ${typeof prop} found.`);
+      }
+  
+      // do the custom check here
+      // using a simple regex
+      if (prop.length < 5 || !prop.match(new RegExp(/^https?:\/\//))) {
+        return new Error(`Invalid prop, ${propName} must be min length 5 and begin http(s)://. Validation Failed.`);
+      }
+    },
+    rating: PropTypes.number,
+    description: PropTypes.string
   }))
 };
