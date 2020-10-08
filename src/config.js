ee4b1f724a526132c0eef298181f0f850d1ea3f4
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2020-10-08 15:07:21 -0400

    added alias in vercel.json

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api/bookmarks",
+  API_ENDPOINT: "https://thawing-hollows-57701.herokuapp.com/",
   API_KEY: process.env.REACT_APP_API_KEY,
 }
